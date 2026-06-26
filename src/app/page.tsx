c372184a4e12bc1ba4164d24806a8bf316d076d6
Gabriel Henrique <gabrielhs2@al.insper.edu.br>
2026-06-26 12:43:40 -0300

    fix: my first app

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center text-5xl font-bold mb-4">My first website</h1>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded flex justify-center items-center m-auto">Click me</button>
+    </main>
+  );
+}
